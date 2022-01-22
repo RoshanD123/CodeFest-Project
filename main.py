4bc19c44749d0ea5475f90e7cb529c5a92f9b84c
Roshan_D <68422709+RoshanD123@users.noreply.github.com>
2022-01-22 09:01:41 +0000

    New system of categorising symptoms

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ Substance use disorders
 Tuberculosis
 '''
 def symptoms():
-  print("for the questions, if you dont find it applicable, then just put '-'")
+  a="yes"
+  b="no"
   cough = str(input("Have you been coughing frequently?"))
   runny_nose = str(input("Is your nose runny?"))
   sneezing = str(input("Have you been sneezing frequently?"))
@@ -51,7 +52,7 @@ def symptoms():
   Time=int(input("How long did it take for symptoms to show"))
   listof=["Cancer","Chronic kidney disease","Chronic Lung disease","Dementia","Neurological Conditions","Diabetes","Down Syndrome","Heart conditions","HIV","Weakened Immune system","Mental Health conditions","Overweight and obesity","Sickle cell disease","smoking, current of former","substance use disorders","Tuberculosis"]
   print(listof)
-  dis="no"
+  dis=str(input("Have you got any of these diseases?"))
   if dis=="yes" and cough=="yes" and runny_nose=="yes" and sneezing=="yes" and shortness_of_breath=="yes" and sore_throat=="yes"and fever=="yes" and night_sweats=="yes" and chills=="yes" and headache=="yes" and loss_of_smell=="yes"and fatigue=="yes":
     print("You are in an extreme chronic condition and so go to a doctor as soon as possible.")
   
@@ -60,19 +61,23 @@ def symptoms():
   
   elif dis=="no" and cough=="yes" or runny_nose=="yes" or sneezing=="yes" or shortness_of_breath=="yes" or sore_throat=="yes"or fever=="yes" or night_sweats=="yes" or chills=="yes" or headache=="yes" or loss_of_smell=="yes" or fatigue=="yes":
     print("Please continue")
-    if night_sweats=="yes":
-      print("50% chance  for Omnicron Variant of COVID")
-    elif night_sweats=="no":
-            print("50% chance for not having Omnicron Varinat COVID")
-    elif sneezing=="no":
-            print("100% chance for not having Delta Variant COVID or Flu")
-    elif sneezing=="yes":
-            print("100% chance to have Common Cold or Omnicron variant COVID")
-    elif shortness_of_breath=="yes":
-            print("75% chance for having Delta Variant of COVID")
-  
+    if cough==a and runny_nose==a and sneezing==b and shortness_of_breath==a and sore_throat==a and fever==a and night_sweats==b and chills==a and headache==a and loss_of_smell==a and fatigue==a and Time==4 or Time==5:
+      print("You are most likely to have Delta Variant of COVID")
+    elif cough==a and runny_nose==a and sneezing==a and shortness_of_breath==b and sore_throat==a and fever==a and night_sweats==a and chills==a and headache==a and loss_of_smell==a and fatigue==a and Time==2 or Time==3:
+      print("You are most likely to have Omnicron Variant of COVID")
+    elif cough==a and runny_nose==a and sneezing==b and shortness_of_breath==b and sore_throat==a and fever==a and night_sweats==b and chills==a and headache==a and loss_of_smell==b and fatigue==a and Time==2 or Time==4:
+      print("You are most likely to have the flu")
+    elif cough==a and runny_nose==a and sneezing==a and shortness_of_breath==b and sore_throat==a and fever==a and night_sweats==b and chills==b and headache==a and loss_of_smell==b and fatigue==a and Time==1 or Time==3:
+      print("You are most likely to have the common cold")
+    else:
+      if cough==b and runny_nose==a and sneezing==a and shortness_of_breath==b and sore_throat==a and fever==b and night_sweats==b and chills==b and headache==a and loss_of_smell==b and fatigue==a and Time==2 or Time==3:
+        print("You might have Omnicron Variant of COVID")
+      elif cough==a and runny_nose==b and sneezing==b and shortness_of_breath==b and sore_throat==b and fever==a and night_sweats==b and chills==a and headache==a and loss_of_smell==b and fatigue==a and Time==2 or Time==4:
+        print("You might have the flu")
+      elif cough==a and runny_nose==a and sneezing==b and shortness_of_breath==b and sore_throat==a and fever==a and night_sweats==b and chills==b and headache==b and loss_of_smell==a and fatigue==b and Time==1 or Time==3:
+        print("You might have the common cold")
   elif dis=="no" and cough=="no" and runny_nose=="no" and sneezing=="no" and shortness_of_breath=="no" and sore_throat=="no" and fever=="no" and night_sweats=="no" and chills=="no" and headache=="no" and loss_of_smell=="no" and fatigue=="no":
-    print("You are in no danger, but be careful and follow all COVID protocols")
+    print("You are in no danger, but be careful and follow all COVID protocols to ensure your safety and the safety of others around you")
   
 
   
