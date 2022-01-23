a3c82a5ea820bbdf50a1f1db17ae1f7a0fb728dc
Roshan_D <68422709+RoshanD123@users.noreply.github.com>
2022-01-23 04:12:32 +0000

    Final Part 6

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ This Program is used as a COVID predictor
 '''
 
 
-def travel(x):
+def travel(x,y):
     acns = ["yes", "no"]
     th = str(
         input("In the last two to three weeks, have you travelled anywhere? "))
@@ -31,7 +31,7 @@ def travel(x):
                                 "Please enter approximately how many people were there - "
                             ))
                     except ValueError:
-                        print("Sorry, I didn't understand that. ")
+                        print("Sorry, I didn't understand that.")
                         continue
                         if peeps < 0:
                             print(
@@ -41,61 +41,60 @@ def travel(x):
                         else:
                             break
                     if peeps >= 55 and not x == "none":
-                        print("\nYou are at a higher risk to have", x,
-                              "please go to a doctor to keep yourself safe")
+                        print("Dear",y,", you are at a higher risk to have",x,"please go to a doctor to keep yourself safe")
                         print(
-                            "\nThank you so much for using our application.  Hope you get better soon!"
+                            "\nThank you so much for using our application.  Hope you stay safe,(take a COVID test as well)",y,"!"
                         )
                         break
 
                     elif peeps >= 55 and x == "none":
                         print(
-                            "You have a higher risk of catching covid in general, take a test(COVID test) to be sure"
+                            "Dear",y,", you have a higher risk of catching covid in general, take a test(COVID test) to be sure"
                         )
                         print(
-                            "\nThank you so much for using our application.  Hope you get better soon!"
+                            "\nThank you so much for using our application.  Hope you stay safe,(take a COVID test as well)",y,"!"
                         )
                         break
 
                     elif peeps < 55 and x == "none":
                         print(
-                            "Good.  Take care of yourself.  Though there were few people, you still may have a risk of COVID-19. "
+                            "Dear",y,",  Take care of yourself.  Though there were few people, you still may have a risk of COVID-19. "
                         )
                         print(
-                            "\nThank you so much for using our application.  Hope you get better soon!"
+                            "\nThank you so much for using our application.  Hope you stay safe(take a COVID test as well),",y,"!"
                         )
                         break
 
                     else:
                         print(
-                            "Good, you have a lower risk of having", x,
+                            "Dear",y,", you have a lower risk of having", x,
                             ", but from now on, avoid crowds to ensure your safety"
                         )
                         print(
-                            "\nThank you so much for using our application.  Hope you get better soon!"
+                            "\nThank you so much for using our application.  Hope you get better soon and stay safe(take a COVID test as well)",y,"!"
                         )
                         break
             else:
                 print(
-                    "Good job for avoiding mass gatherings, but ensure to stay at home and maintain all social protocols at the place you are at. "
+                    "Dear",y,",Good job for avoiding mass gatherings, but ensure to stay at home and maintain all social protocols at the place you are at. "
                 )
                 print(
-                    "\nThank you so much for using our application.  Hope you get better soon!"
+                    "\nThank you so much for using our application.  Hope you get better soon(take a COVID test as well)",y,"!"
                 )
         else:
             print(
-                "Good Job, but now follow all COVID protocols and dont go to large crowds"
+                "Dear",y,",Good Job, but now follow all COVID protocols and don't go to large crowds"
             )
             print(
-                "\nThank you so much for using our application.  Hope you get better soon!"
+                "\nThank you so much for using our application.  Hope you get better soon(take a COVID test as well)",y,"!"
             )
 
 
-def symptoms():
+def symptoms(y):
     a = "yes"
     b = "no"
     d = "Delta Variant of COVID"
-    o = "Omnicron Variant of COVID"
+    o = "omicron Variant of COVID"
     f = "Flu"
     c = "Common Cold"
     acns = ["yes", "no", "YES", "NO", "YEs", "No", "Yes", "yEs", "nO", "yES"]
@@ -218,59 +217,61 @@ def symptoms():
         "Weakened Immune system", "Mental Health conditions",
         "Overweight and obesity", "Sickle cell disease",
         "Smoking, current or former", "Substance use disorders",
-        "\nTuberculosis"
+        "Tuberculosis"
     ]
     print(listof)
     dis = str(input("Have you got any of these diseases[Yes/No]? "))
+    z="chronic condition"
     if dis == "yes" and cough == "yes" and runny_nose == "yes" and sneezing == "yes" and shortness_of_breath == "yes" and sore_throat == "yes" and fever == "yes" and night_sweats == "yes" and chills == "yes" and headache == "yes" and loss_of_smell == "yes" and fatigue == "yes":
         print(
             "You are in an extreme chronic condition and so go to a doctor as soon as possible. "
         )
-        travel(v)
+        travel(z,y)
     elif dis == "yes" and cough == "no" and runny_nose == "no" and sneezing == "no" and shortness_of_breath == "no" and sore_throat == "no" and fever == "no" and night_sweats == "no" and chills == "no" and headache == "no" and loss_of_smell == "no" and fatigue == "no":
         print(
             "Be careful as you are in dangerous condition, and maintain COVID protocols all the time to ensure your safety. "
         )
-        travel(v)
+        travel(v,y)
 
     elif dis == "no" and cough == "yes" or runny_nose == "yes" or sneezing == "yes" or shortness_of_breath == "yes" or sore_throat == "yes" or fever == "yes" or night_sweats == "yes" or chills == "yes" or headache == "yes" or loss_of_smell == "yes" or fatigue == "yes":
         print("Please continue")
         if cough == a and runny_nose == a and sneezing == b and shortness_of_breath == a and sore_throat == a and fever == a and night_sweats == b and chills == a and headache == a and loss_of_smell == a and fatigue == a and Time == 4 or Time == 5:
             print("You are most likely to have Delta Variant of COVID")
-            travel(d)
+            travel(d,y)
         elif cough == a and runny_nose == a and sneezing == a and shortness_of_breath == b and sore_throat == a and fever == a and night_sweats == a and chills == a and headache == a and loss_of_smell == a and fatigue == a and Time == 2 or Time == 3:
-            print("You are most likely to have Omnicron Variant of COVID")
-            travel(o)
+            print("You are most likely to have omicron Variant of COVID")
+            travel(o,y)
 
         elif cough == a and runny_nose == a and sneezing == b and shortness_of_breath == b and sore_throat == a and fever == a and night_sweats == b and chills == a and headache == a and loss_of_smell == b and fatigue == a and Time == 2 or Time == 3:
             print("You are most likely to have the flu")
-            travel(f)
+            travel(f,y)
 
         elif cough == a and runny_nose == a and sneezing == a and shortness_of_breath == b and sore_throat == a and fever == a and night_sweats == b and chills == b and headache == a and loss_of_smell == b and fatigue == a and Time == 1 or Time == 3:
             print("You are most likely to have the common cold")
             travel(c)
         else:
             if cough == b and runny_nose == a and sneezing == a and shortness_of_breath == b and sore_throat == a and fever == b and night_sweats == b and chills == b and headache == a and loss_of_smell == b and fatigue == a and Time == 2 or Time == 3:
-                print("You might have Omnicron Variant of COVID")
-                travel(o)
+                print("You might have omicron Variant of COVID")
+                travel(o,y)
 
             elif cough == a and runny_nose == b and sneezing == b and shortness_of_breath == b and sore_throat == b and fever == a and night_sweats == b and chills == a and headache == a and loss_of_smell == b and fatigue == a and Time == 2 or Time == 4:
                 print("You might have the flu")
-                travel(f)
+                travel(f,y)
 
             elif cough == a and runny_nose == a and sneezing == b and shortness_of_breath == b and sore_throat == a and fever == a and night_sweats == b and chills == b and headache == b and loss_of_smell == a and fatigue == b and Time == 1 or Time == 3:
                 print("You might have the common cold")
-                travel(c)
+                travel(c,y)
             else:
                 print(
                     "Your inputs did not match any of the symptoms of any of the diseases specifically"
                 )
+                travel(v,y)
 
     elif dis == "no" and cough == "no" and runny_nose == "no" and sneezing == "no" and shortness_of_breath == "no" and sore_throat == "no" and fever == "no" and night_sweats == "no" and chills == "no" and headache == "no" and loss_of_smell == "no" and fatigue == "no":
         print(
             "You are in no danger, but be careful and follow all COVID protocols to ensure your safety and the safety of others around you"
         )
-        travel(v)
+        travel(v,y)
 
 
 print("Welcome to the COVID Predictor")
@@ -293,7 +294,6 @@ if age >= 60:
     while True:
         try:
             doses = int(input("Please enter the number of doses: "))
-            break
         except ValueError:
             print("Sorry, Invalid Input. ")
             continue
@@ -304,7 +304,7 @@ if age >= 60:
             )
             continue
         else:
-            symptoms()
+            symptoms(Name)
             break
 
 elif age >= 18:
@@ -324,9 +324,16 @@ elif age >= 18:
             if doses == 1:
                 print("Ensure that you take your", str(2 - doses),
                       "dose, and Follow corona protocols at all times. ")
-                symptoms()
+                symptoms(Name)
                 break
             elif doses == 2:
                 print("Follow all corona protocols at all times")
-                symptoms()
+                symptoms(Name)
                 break
+else:
+  if age<15:
+    print("You can get 0 doses")
+    symptoms(Name)
+  elif age>=15 and age<=18:
+    print("You can get 1 dose of vaccination")
+    symptoms(Name)
